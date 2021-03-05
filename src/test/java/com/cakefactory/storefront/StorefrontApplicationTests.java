e275b131d1d87dd75f491f08f1e871134d6e7d4e
Michael Becker <michael.f.becker@gmail.com>
2021-03-04 21:07:43 -0500

    Starting work on milestone 2
    Reviewing JUnit test.
    Added required new dependencies.

--- src/test/java/com/cakefactory/storefront/StorefrontApplicationTests.java
+++ src/test/java/com/cakefactory/storefront/StorefrontApplicationTests.java
@@ -1,5 +1,7 @@
 package com.cakefactory.storefront;
 
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,13 @@ class StorefrontApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+
+
+	@Test
+	void assertThisIsFalse() {
+		Assertions.assertFalse(false);
 	}
 
 }
